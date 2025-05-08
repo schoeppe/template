93a874deb444dacc7eafd31d06ab1bd9c07be4c8
Florian Schöppe <florian@schoeppe.info>
2025-05-08 13:28:08 +0200

    Replace usage of constant TYPO3_MODE with TYPO3
    + move ExtensionManagementUtility call into TCA configuration

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,5 +1,12 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3') or die();
+
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'pages',
+    'module',
+    '4',
+    'after:--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.miscellaneous;miscellaneous'
+);
 
 $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
     'label' => 'LLL:EXT:template/Resources/Private/Language/locallang_db.xlf:plugin.contains.menu',
--- ext_tables.php
+++ ext_tables.php
@@ -1,28 +1,17 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-    'pages',
-    'module',
-    '4',
-    'after:--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.palettes.miscellaneous;miscellaneous'
-);
+$icons = [
+    'apps-pagetree-folder-contains-section' => 'EXT:template/Resources/Public/Backend/apps-pagetree-section.svg',
+    'apps-pagetree-folder-contains-singlepage' => 'EXT:template/Resources/Public/Backend/apps-pagetree-singlepage.svg',
+];
 
-if (TYPO3_MODE === 'BE') {
-    $icons = [
-        'apps-pagetree-folder-contains-section' => 'EXT:template/Resources/Public/Backend/apps-pagetree-section.svg',
-        'apps-pagetree-folder-contains-singlepage' => 'EXT:template/Resources/Public/Backend/apps-pagetree-singlepage.svg',
-    ];
-
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($icons as $identifier => $path) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            ['source' => $path]
-        );
-    }
+/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+foreach ($icons as $identifier => $path) {
+    $iconRegistry->registerIcon(
+        $identifier,
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => $path]
+    );
 }
