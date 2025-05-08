e1153fabb79490c8651bb21482e7c58cca25e66d
Florian Schöppe <florian@schoeppe.info>
2025-05-08 13:26:39 +0200

    Add keys for module item configuration

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,15 +2,15 @@
 defined('TYPO3_MODE') || die();
 
 $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-    'LLL:EXT:template/Resources/Private/Language/locallang_db.xlf:plugin.contains.menu',
-    'section',
-    'apps-pagetree-folder-contains-section',
+    'label' => 'LLL:EXT:template/Resources/Private/Language/locallang_db.xlf:plugin.contains.menu',
+    'value' => 'section',
+    'icon' => 'apps-pagetree-folder-contains-section',
 ];
 $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-section'] = 'apps-pagetree-folder-contains-section';
 
 $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-    'Singlepage Parent',
-    'singlepage',
-    'apps-pagetree-folder-contains-singlepage',
+    'label' => 'Singlepage Parent',
+    'value' => 'singlepage',
+    'icon' => 'apps-pagetree-folder-contains-singlepage',
 ];
 $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-singlepage'] = 'apps-pagetree-folder-contains-singlepage';
